4ee60800bfc437042f8b925b98e5a46f6d93edf2
Raskovan <alexey.katalkin@gmail.com>
2017-01-31 23:39:04 -0500

    Epoch

---
+++ EpochButton/FISEpochViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISEpochViewController.h
+//  EpochButton
+//
+//  Created by Katala on 1/31/17.
+//  Copyright © 2017 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISEpochViewController : UIViewController
+
+@end
